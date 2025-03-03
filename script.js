f71ecb7879346befbca382fbb5ec36409b60fd7f
New <naths.raf@gmail.com>
2025-03-03 14:05:41 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,369 @@
+let ListNext = [
+    {
+        name: "Valentin",
+        picture: "images/67C67D50-0F90-4322-A308-31EC665AA3FD-510x376.webp",
+        years: 22,
+        single: true,
+        anecdotes: [
+            {
+                favoritefood: "Aime le poulet halal",
+                favoritething: "A déjà léché son orteil",
+                bodycount: 2,
+            },
+        ],
+        ist: ["Chlamydia", "Gonorrhée", "Syphilis"],
+    },
+    {
+        name: "Emilien",
+        picture: "images/j73m-OUVQHUmaccNUjsVncOUyi72mC6tNnMKUNpPTKWqWNVgEqbDhGi_OTiHEq5TAkHwGMBLxSKscPXiuC9qUU1ODOrC9rsHa0SNrw.avif",
+        years: 23,
+        single: true,
+        anecdotes: [
+            {
+                favoritefood: "Aime la pizza hawaïenne",
+                favoritething: "Fait du kayak sans rames",
+                bodycount: 55,
+            },
+        ],
+        ist: ["Hépatite B", "HPV", "Herpès génital"],
+    },
+    {
+        name: "Patoche",
+        picture: "images/NbZCjDEsva91FW1W.jpg",
+        years: 48,
+        single: false,
+        anecdotes: [
+            {
+                favoritefood: "Aime le Ricard à 14h",
+                favoritething: "Pense que l'alcool c'est cool",
+                bodycount: 0,
+            },
+        ],
+        ist: ["Syphilis", "Chlamydia", "Trichomonase"],
+    },
+    {
+        name: "Brad",
+        picture: "images/1736752517-fj5cplp.webp",
+        years: 45,
+        single: true,
+        anecdotes: [
+            {
+                favoritefood: "Aime le pigeon farci",
+                favoritething: "Arnaque des vieilles sur internet",
+                bodycount: 256,
+            },
+        ],
+        ist: ["VIH", "Herpès génital", "Gonorrhée"],
+    },
+    {
+        name: "Balthazar",
+        picture: "images/Balthazar.webp",
+        years: 28,
+        single: true,
+        anecdotes: [
+            {
+                favoritefood: "Aime les flageolets",
+                favoritething: "Collectionne les cassettes porn de 1970",
+                bodycount: 890,
+            },
+        ],
+        ist: ["HPV", "Hépatite C", "Chlamydia"],
+    },
+    {
+        name: "Sam",
+        picture: "images/artworks-5V5Fw7hspQ2yP8PL-yxsskw-t500x500.jpg",
+        years: 21,
+        single: true,
+        anecdotes: [
+            {
+                favoritefood: "Aime les pilules de créatine",
+                favoritething: "Fan de bodybuilding et de seringues",
+                bodycount: 10,
+            },
+        ],
+        ist: ["Hépatite B", "Gonorrhée", "Herpès génital"],
+    },
+    {
+        name: "Tristan",
+        picture: "images/interview.-ses-debuts-tele-realite-carriere-mannequin-max-couples-parfaits-confie.png",
+        years: 21,
+        single: false,
+        anecdotes: [
+            {
+                favoritefood: "Aime les pâtes au pesto",
+                favoritething: "A une passion pour les chaînes de vélo",
+                bodycount: 4,
+            },
+        ],
+        ist: ["Syphilis", "VIH", "Trichomonase"],
+    },
+    {
+        name: "Julien",
+        picture: "images/Julien_Tanti.webp",
+        years: 29,
+        single: true,
+        anecdotes: [
+            {
+                favoritefood: "Aime le curry de poulet",
+                favoritething: "Fan de Sylvain Durif Prime",
+                bodycount: 7,
+            },
+        ],
+        ist: ["Herpès génital", "Chlamydia", "HPV"],
+    },
+    {
+        name: "Oussamma",
+        picture: "images/1680188584176.jpg",
+        years: 27,
+        single: false,
+        anecdotes: [
+            {
+                favoritefood: "Aime les buffets à volonté",
+                favoritething: "A déjà coulé un restaurant chinois",
+                bodycount: 1,
+            },
+        ],
+        ist: ["Hépatite C", "VIH", "Gonorrhée"],
+    },
+    {
+        name: "Gaspard",
+        picture: "images/Gaspard.webp",
+        years: 17,
+        single: true,
+        anecdotes: [
+            {
+                favoritefood: "Aime les poireaux",
+                favoritething: "S'est téléporté en Jordanie pour la naissance du Christ",
+                bodycount: 3,
+            },
+        ],
+        ist: ["Trichomonase", "Chlamydia", "HPV"],
+    },
+    {
+        name: "Samir",
+        picture: "images/valentin-leonard.jpg",
+        years: 29,
+        single: true,
+        anecdotes: [
+            {
+                favoritefood: "Aime les petits pois carottes",
+                favoritething: "A fait livreur Uber Eats au Kazakhstan",
+                bodycount: 6,
+            },
+        ],
+        ist: ["Herpès génital", "Syphilis", "VIH"],
+    },
+    {
+        name: "Anis",
+        picture: "images/didier-bourdon-dans-un-sketch-des-inconnus.webp",
+        years: 26,
+        single: false,
+        anecdotes: [
+            {
+                favoritefood: "Aime les chaussons aux pommes",
+                favoritething: "A déjà catfish une aveugle sur tinder",
+                bodycount: 72,
+            },
+        ],
+        ist: ["Hépatite B", "Chlamydia", "Trichomonase"],
+    },
+    {
+        name: "Nadir",
+        picture: "images/Tristanjouve.webp",
+        years: 23,
+        single: true,
+        anecdotes: [
+            {
+                favoritefood: "Aime le tofu vapeur",
+                favoritething: "A une phobie inexplicable des grands-mères",
+                bodycount: 3,
+            },
+        ],
+        ist: ["Gonorrhée", "Herpès génital", "VIH"],
+    },
+    {
+        name: "Youssef",
+        picture: "images/HPKHD2ZVANE4ZLZOVOG44CI5JI.avif",
+        years: 31,
+        single: true,
+        anecdotes: [
+            {
+                favoritefood: "Aime les kebabs végé",
+                favoritething: "A déjà menti sur son test de pureté",
+                bodycount: 16,
+            },
+        ],
+        ist: ["Syphilis", "HPV", "Hépatite B"],
+    },
+    {
+        name: "Marc",
+        picture: "images/1294c99f44164c3611d016819ca5b5fc7c74504d.webp",
+        years: 28,
+        single: false,
+        anecdotes: [
+            {
+                favoritefood: "Aime Marc ",
+                favoritething: "Passe 2 heures à se regarder dans le iroir",
+                bodycount: 29,
+            },
+        ],
+        ist: ["Chlamydia", "Herpès génital", "Hépatite C"],
+    },
+
+]
+
+let ImageCelib = "images/heart-broken-svgrepo-com.png";
+
+let ImageCouple = "images/heart-svgrepo-com.png";
+
+let ImageVirus = "images/virus-covid-19-svgrepo-com.png";
+
+let buttonCardFormat = document.getElementById("SwitchFormat");
+
+buttonCardFormat.addEventListener("click", ChangeCardFormat)
+
+let smallCard = true;
+
+let bigCard = false;
+
+
+
+function creerCarte(element){
+    
+
+    let newCard = document.createElement("div");
+        newCard.classList.add("NextCard");
+
+        let pictureCard = document.createElement("img");
+        pictureCard.classList.add("vignette-card");
+        pictureCard.src = element.picture;
+
+        let infoTitle = document.createElement("div");
+        infoTitle.classList.add("title")
+
+        let cardName = document.createElement("h1");
+        cardName.innerText = element.name;
+        let cardYears = document.createElement("h2");
+        let cardNumber = element.years;
+        cardYears.innerText = cardNumber + " ans";
+  
+        infoTitle.appendChild(cardName)
+        infoTitle.appendChild(cardYears)
+
+        let cardInfo = document.createElement("div");
+        cardInfo.classList.add("CardInfo");
+
+        let imgStatut = document.createElement("img");
+
+        if(element.single == true){
+            imgStatut.src = ImageCelib
+        }
+        else if(element.single === false){
+            imgStatut.src = ImageCouple
+        }
+
+        cardInfo.appendChild(imgStatut)
+
+        let cardAnecdotes = document.createElement("ol");
+
+        let i = 0
+
+        for(i = 0; i< element.anecdotes.length ; i+=1)
+        {
+
+            let anecdote1 = document.createElement("li")
+             anecdote1.innerHTML = element.anecdotes[i].favoritefood;
+
+             let anecdote2 = document.createElement("li")
+             anecdote2.innerHTML = element.anecdotes[i].favoritething;
+
+             let anecdote3 = document.createElement("li")
+             anecdote3.innerHTML = "Bodycount : " + element.anecdotes[i].bodycount ;
+
+             cardAnecdotes.appendChild(anecdote1)
+             cardAnecdotes.appendChild(anecdote2)
+             cardAnecdotes.appendChild(anecdote3)
+        }
+
+        let cardImgVirus = document.createElement("img");
+        cardImgVirus.src = ImageVirus;
+
+        let cardListVirus = document.createElement("ul");
+
+        let m = 0
+
+        for(m = 0; m<element.ist.length; m++)
+        {
+          let maladie1 = document.createElement("li")
+          maladie1.innerText = element.ist[m]
+
+          cardListVirus.appendChild(maladie1)
+        }
+
+        cardInfo.appendChild(cardAnecdotes) ;
+        cardInfo.appendChild(cardImgVirus);
+        cardInfo.appendChild(cardListVirus);
+        
+       
+        
+        newCard.appendChild(pictureCard);
+        newCard.appendChild(infoTitle);
+        newCard.appendChild(cardInfo);
+        let v = document.querySelector("main");
+        v.appendChild(newCard)
+
+        newCard.addEventListener("click", ChangeCardFormat)
+
+}
+
+function CreerPlusieursCartes(){
+    ListNext.forEach(element=> creerCarte(element));
+}
+
+CreerPlusieursCartes();
+
+
+let boxView = document.getElementById("galery")
+
+function ChangeCardFormat(){
+   let cardFormat = document.getElementsByClassName("CardInfo"); 
+   if(smallCard == true)
+    {
+        bigCard = true;
+        smallCard = false;
+        console.log("switch1");
+       
+        
+ for(let i = 0; i < cardFormat.length ; i++ )
+        {
+            cardFormat[i].classList.add("CardInfoMasked");
+        }
+let boxView = document.getElementById("galery")
+boxView.classList.remove("carrousel");
+        
+        
+    }
+    else if( bigCard === true && smallCard === false)
+    {
+        smallCard = true;
+        console.log("switch2");
+        bigCard = true;
+
+        for(let i = 0; i < cardFormat.length ; i++ )
+        {
+            cardFormat[i].classList.remove("CardInfoMasked");
+        }
+        
+        boxView.classList.add("carrousel");
+    }
+    
+}
+
+
+
+
+    
+
+     
+   
+
