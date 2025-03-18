43425944f5b3546126b3570aaaed3711f898f863
New <naths.raf@gmail.com>
2025-03-18 09:34:42 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -228,93 +228,68 @@ let bigCard = false;
 
 
 
-function creerCarte(element){
-    
-
+function creerCarte(element) {
     let newCard = document.createElement("div");
-        newCard.classList.add("NextCard");
+    newCard.classList.add("NextCard");
 
-        let pictureCard = document.createElement("img");
-        pictureCard.classList.add("vignette-card");
-        pictureCard.src = element.picture;
+    let imgStatut
 
-        let infoTitle = document.createElement("div");
-        infoTitle.classList.add("title")
+    if(element.single == true)
+    {
+        imgStatut = ImageCelib;
+    }
+    else{
+        imgStatut = ImageCouple;
+    
+    }
 
-        let cardName = document.createElement("h1");
-        cardName.innerText = element.name;
-        let cardYears = document.createElement("h2");
-        let cardNumber = element.years;
-        cardYears.innerText = cardNumber + " ans";
-  
-        infoTitle.appendChild(cardName)
-        infoTitle.appendChild(cardYears)
+    let contenuPrincipal = `
+        <img class="vignette-card" src="${element.picture}" alt=""/>
+        <div class="title">
+            <h1>${element.name}</h1>
+            <h2><span>48</span> ans</h2>
+        </div>
+        <div class="CardInfo">
+            <img src="${imgStatut}" />
+    `;
 
-        let cardInfo = document.createElement("div");
-        cardInfo.classList.add("CardInfo");
+    let contenuAnecdotes = "<ol>";
 
-        let imgStatut = document.createElement("img");
+    for (let index = 0; index < element.anecdotes.length; index += 1) {
+        contenuAnecdotes += `
+            <li>${element.anecdotes[index].favoritefood}</li>
+            <li>${element.anecdotes[index].favoritething}</li>
+            <li>Bodycount : ${element.anecdotes[index].bodycount}</li>
+        `;
+    }
 
-        if(element.single == true){
-            imgStatut.src = ImageCelib
-        }
-        else if(element.single === false){
-            imgStatut.src = ImageCouple
-        }
+    contenuAnecdotes += "</ol>";
 
-        cardInfo.appendChild(imgStatut)
 
-        let cardAnecdotes = document.createElement("ol");
+    let contenuVirus = `
+     <span>
+                <svg width="64px" height="64px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M18 12C18 15.3137 15.3137 18 12 18M18 12C18 8.68629 15.3137 6 12 6M18 12H21M12 18C8.68629 18 6 15.3137 6 12M12 18V21M6 12C6 8.68629 8.68629 6 12 6M6 12H3M12 6V3M21 10.5V13.5M3 10.5V13.5M10.5 3H13.5M10.5 21H13.5M17.3037 4.5752L19.425 6.69652M4.5752 17.3037L6.69652 19.425M18.3643 5.63574L16.2429 7.75706M7.75781 16.2422L5.63649 18.3635M4.5752 6.69629L6.69652 4.57497M17.3037 19.4248L19.425 17.3035M5.63574 5.63574L7.75706 7.75706M16.2422 16.2422L18.3635 18.3635M14 14H14.01M10.5 11.5H10.51M11.5 11.5C11.5 12.0523 11.0523 12.5 10.5 12.5C9.94772 12.5 9.5 12.0523 9.5 11.5C9.5 10.9477 9.94772 10.5 10.5 10.5C11.0523 10.5 11.5 10.9477 11.5 11.5Z" stroke="#9e0000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path> </g></svg>
+            </span>` ; 
 
-        let i = 0
 
-        for(i = 0; i< element.anecdotes.length ; i+=1)
-        {
+            let contenuMaladie = "<ol>";
 
-            let anecdote1 = document.createElement("li")
-             anecdote1.innerHTML = element.anecdotes[i].favoritefood;
+            for(let number = 0; number< element.ist.length; number ++)
+            {
+               contenuMaladie += ` <li>${element.ist[number]}</li>`
+            
+            }
 
-             let anecdote2 = document.createElement("li")
-             anecdote2.innerHTML = element.anecdotes[i].favoritething;
+            contenuMaladie += "</ol>";
 
-             let anecdote3 = document.createElement("li")
-             anecdote3.innerHTML = "Bodycount : " + element.anecdotes[i].bodycount ;
+    newCard.innerHTML = contenuPrincipal + contenuAnecdotes + contenuVirus + contenuMaladie;
 
-             cardAnecdotes.appendChild(anecdote1)
-             cardAnecdotes.appendChild(anecdote2)
-             cardAnecdotes.appendChild(anecdote3)
-        }
+    document.querySelector("main").appendChild(newCard);
 
-        let cardImgVirus = document.createElement("img");
-        cardImgVirus.src = ImageVirus;
+    newCard.addEventListener("click", ChangeCardFormat);
 
-        let cardListVirus = document.createElement("ul");
+};
 
-        let m = 0
-
-        for(m = 0; m<element.ist.length; m++)
-        {
-          let maladie1 = document.createElement("li")
-          maladie1.innerText = element.ist[m]
-
-          cardListVirus.appendChild(maladie1)
-        }
-
-        cardInfo.appendChild(cardAnecdotes) ;
-        cardInfo.appendChild(cardImgVirus);
-        cardInfo.appendChild(cardListVirus);
-        
-       
-        
-        newCard.appendChild(pictureCard);
-        newCard.appendChild(infoTitle);
-        newCard.appendChild(cardInfo);
-        let v = document.querySelector("main");
-        v.appendChild(newCard)
-
-        newCard.addEventListener("click", ChangeCardFormat)
-
-}
 
 function CreerPlusieursCartes(){
     ListNext.forEach(element=> creerCarte(element));
